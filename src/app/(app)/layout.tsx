5bc96a8e981b8ed019bf75107812bb2839d75c4a
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-02-03 15:48:21 -0300

    chore: config project structure

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+
+import { Header } from '../components/header'
+
+export default function StoreLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
+
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">{children}</div>
+    </div>
+  )
+}
