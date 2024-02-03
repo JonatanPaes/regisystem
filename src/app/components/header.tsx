d5657384c9be3d925323db4cf0d6f05b1b8d435c
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-02-03 15:46:23 -0300

    feat: add component header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,33 @@
+import { MenuIcon } from 'lucide-react'
+import Link from 'next/link'
+
+import { SideMenu } from './side-menu'
+import { Button } from './ui/button'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+
+export function Header() {
+  return (
+    <div className="border-b">
+      <div className="flex h-20 items-center justify-between px-6">
+        <Link
+          href="/"
+          className="text-xl font-extrabold text-accent-foreground"
+        >
+          RegiSystem
+        </Link>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon size={16} />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
+      </div>
+    </div>
+  )
+}
