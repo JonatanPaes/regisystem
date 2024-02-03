56a386d4791c9ee91c57ff1dc4af6154f453f0d0
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-02-03 12:15:47 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <h1>Hello CartSys</h1>
+  return (
+    <div>
+      <h1>Hello CartSys</h1>
+
+      <Button>Testando botao</Button>
+    </div>
+  )
 }
