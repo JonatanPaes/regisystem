4309f81d91b8c5ffd3d34b921b8250b271d6605c
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2024-02-06 20:52:09 -0300

    feat: page finish order

--- src/app/(app)/assistant/product/[id]/payment/address/page.tsx
+++ src/app/(app)/assistant/product/[id]/payment/address/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { ButtonBack } from '@/app/components/button-back'
+import { MessageError } from '@/app/components/message-error'
 import { Button } from '@/app/components/ui/button'
 import {
   Dialog,
@@ -21,12 +23,14 @@ import { Separator } from '@/app/components/ui/separator'
 import { useAssistant } from '@/contexts/assistant-context'
 
 const addressForm = z.object({
-  cep: z.string().min(8),
-  address: z.string().min(10),
-  residenceNumber: z.string().min(1),
-  neighborhood: z.string().min(3),
-  city: z.string().min(3),
-  state: z.string().min(2),
+  cep: z.string().min(8, { message: 'O CEP é obrigatório.' }),
+  address: z.string().min(1, { message: 'O Endereço é obrigatório.' }),
+  residenceNumber: z
+    .string()
+    .min(1, { message: 'O Número da residência é obrigatório.' }),
+  neighborhood: z.string().min(1, { message: 'O Bairro é obrigatório.' }),
+  city: z.string().min(1, { message: 'A cidade é obrigatória.' }),
+  state: z.string().min(1, { message: 'O Estado é obrigatório.' }),
 })
 
 type AddressForm = z.infer<typeof addressForm>
@@ -41,16 +45,17 @@ interface Address {
 }
 
 export default function Address() {
-  const { client, product } = useAssistant()
+  const { push } = useRouter()
+
+  const { client, product, payment } = useAssistant()
 
   const [isResumeOpen, setIsResumeOpen] = useState(false)
-
   const [address, setAddress] = useState<Address>()
 
   const {
     register,
     handleSubmit,
-    formState: { isSubmitting },
+    formState: { isSubmitting, errors, isValid },
   } = useForm<AddressForm>({
     resolver: zodResolver(addressForm),
   })
@@ -59,8 +64,8 @@ export default function Address() {
     setAddress(data)
   }
 
-  function handleFinalizePurchase() {
-    const existingOrder = localStorage.getItem('order')
+  function handleFinalizeOrder() {
+    const existingOrder = localStorage.getItem('orders')
 
     const orderStructure = {
       price: product?.price,
@@ -71,13 +76,25 @@ export default function Address() {
 
     let order = []
 
-    if (existingOrder) {
-      order = JSON.parse(existingOrder)
+    try {
+      if (existingOrder) {
+        order = JSON.parse(existingOrder)
+      }
+
+      order.push(orderStructure)
+
+      toast.success('Pedido realizado com sucesso')
+
+      localStorage.setItem('orders', JSON.stringify(order))
+
+      push('/order-success')
+    } catch {
+      toast.error('Erro ao finalizar pedido')
     }
+  }
 
-    order.push(orderStructure)
-
-    localStorage.setItem('order', JSON.stringify(order))
+  function handleFinishOrder() {
+    setIsResumeOpen(!isResumeOpen)
   }
 
   return (
@@ -100,7 +117,13 @@ export default function Address() {
             <div>
               <h2 className="font-bold">{product?.productName}</h2>
 
-              <p className="text-sm text-card-foreground">{product?.price}</p>
+              <p className="text-sm text-card-foreground">
+                {' '}
+                {product?.price.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })}
+              </p>
 
               <span className="text-sm">Qtd: {product?.stock}</span>
             </div>
@@ -110,7 +133,9 @@ export default function Address() {
             <div>
               <h2 className="font-bold">Método de Pagamento</h2>
 
-              <p className="text-sm text-card-foreground">Cartão de Credito</p>
+              <p className="text-sm text-card-foreground">
+                {`${payment?.type === 'card' ? 'Cartão  de credito' : 'Dinheiro'} em ${payment?.installmentNumbers}`}
+              </p>
             </div>
 
             <Separator />
@@ -131,7 +156,7 @@ export default function Address() {
           </div>
 
           <div className="flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2">
-            <Button onClick={handleFinalizePurchase} disabled={isSubmitting}>
+            <Button onClick={handleFinalizeOrder} disabled={isSubmitting}>
               Finalizar Compra
             </Button>
           </div>
@@ -147,6 +172,8 @@ export default function Address() {
             placeholder="EX. 18690000"
             {...register('cep')}
           />
+
+          {errors.cep && <MessageError>{errors.cep.message}</MessageError>}
         </div>
 
         <div className="space-y-2">
@@ -157,6 +184,10 @@ export default function Address() {
             placeholder="Digite o endereço"
             {...register('address')}
           />
+
+          {errors.address && (
+            <MessageError>{errors.address.message}</MessageError>
+          )}
         </div>
 
         <div className="space-y-2">
@@ -167,6 +198,10 @@ export default function Address() {
             placeholder="Digite o número da residência"
             {...register('residenceNumber')}
           />
+
+          {errors.residenceNumber && (
+            <MessageError>{errors.residenceNumber.message}</MessageError>
+          )}
         </div>
 
         <div className="space-y-2">
@@ -177,24 +212,32 @@ export default function Address() {
             placeholder="Digite o bairro"
             {...register('neighborhood')}
           />
+
+          {errors.neighborhood && (
+            <MessageError>{errors.neighborhood.message}</MessageError>
+          )}
         </div>
 
         <div className="space-y-2">
           <Label htmlFor="city">Cidade</Label>
           <Input id="city" type="text" placeholder="" {...register('city')} />
+
+          {errors.city && <MessageError>{errors.city.message}</MessageError>}
         </div>
 
         <div className="space-y-2">
           <Label htmlFor="state">Estado</Label>
           <Input id="state" type="text" placeholder="" {...register('state')} />
+
+          {errors.state && <MessageError>{errors.state.message}</MessageError>}
         </div>
 
         <div className="flex w-full gap-4 md:w-72">
           <ButtonBack />
 
           <Button
-            onClick={() => setIsResumeOpen(!isResumeOpen)}
-            disabled={isSubmitting}
+            onClick={() => handleFinishOrder()}
+            disabled={isSubmitting || !isValid}
             className="w-full"
             type="submit"
           >
